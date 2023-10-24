5995954565fc077d10596af449f183e452208aa8
Hardik Khandelwal <khandelwalhk02@gmail.com>
2023-10-24 22:16:52 +0530

    update deploy.yaml

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,18 @@
 package org.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
+    private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.print("Hello and welcome!");
+
+
     }
 }
