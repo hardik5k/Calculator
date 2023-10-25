1623f2b1007512432562f864f25f8909779d3422
Hardik Khandelwal <khandelwalhk02@gmail.com>
2023-10-25 09:39:38 +0530

    added calculator logic

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,17 +2,93 @@ package org.example;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
-
     private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        logger.info("Start of Execution");
+
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("--Welcome to Calculator--");
+            System.out.println("Choose your operation");
+
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+            System.out.println("5. Exit");
+
+            int option = sc.nextInt();
+
+            if( option == 5 )
+            {
+                logger.info("End of Execution");
+                break;
+            }
+
+            System.out.println("Enter first operand:");
+
+            int x = sc.nextInt();
+
+            System.out.println("Enter second operand:");
+
+            int y = sc.nextInt();
+
+            System.out.println("Result: ");
+            switch(option)
+            {
+                case 1:
+                    System.out.println(add(x,y));
+                    break;
+                case 2:
+                    System.out.println(sub(x,y));
+                    break;
+                case 3:
+                    System.out.println(mul(x,y));
+                    break;
+                case 4:
+                    System.out.println(div(x,y));
+                    break;
+            }
 
 
+        }
+
+    }
+
+    public static int add(int x, int y)
+    {
+        logger.info("START OP: Add");
+        int z = x+y;
+        logger.info("END OP: Add");
+        return z;
+    }
+    public static int mul(int x, int y) {
+        logger.info("START OP: Mul");
+        int z = x*y;
+        logger.info("END OP: Mul");
+        return z;
+    }
+    public static int sub(int x, int y)
+    {
+        logger.info("START OP: Sub");
+        int z = x-y;
+        logger.info("END OP: Sub");
+        return z;
+    }
+    public static int div(int x, int y)
+    {
+        logger.info("START OP: Div");
+        if(y==0) logger.error("Invalid Input");
+        else
+        {
+            int z = x/y;
+            logger.info("END OP: Div");
+            return z;
+        }
+        return -1;
     }
 }
